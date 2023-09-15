6f609ececaa3c746c603a0e17478e3585f129c4b
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2023-09-15 17:01:04 -0300

    Atualizando Repositório
    Atualizando com os exemplos da aula do dia 12/09/2023 e com o README

---
+++ 2023_09_12/exemplo_01.py
@@ -0,0 +1,9 @@
+ano_atual      = 2023
+ano_nascimento = 1974
+idade          = ano_atual - ano_nascimento
+
+print('A sua idade é',idade)
+
+print('Você tem',idade,'Anos')
+
+print('Você nasceu em',ano_nascimento,'e tem',idade,'anos')
---
+++ 2023_09_12/exemplo_02.py
@@ -0,0 +1,14 @@
+ano_atual      = 2023
+ano_nascimento = 1974
+idade          = ano_atual - ano_nascimento
+
+'''
+print('A sua idade é',idade)
+print('Você tem',idade,'anos')
+print('Você nasceu em',ano_nascimento,'e tem',idade,'anos')
+'''
+
+print(f'A sua idade é {idade}\n')
+print(f'Você tem {idade} anos\n')
+print(f'Você nasceu em {ano_nascimento} e tem {idade} anos\n')
+print(f'Você nasceu em {ano_nascimento} \ne tem {idade} anos')
