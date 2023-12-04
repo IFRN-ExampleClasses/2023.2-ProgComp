816f6220c9b94d8ff1dafba4d69948374285264d
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2023-12-04 20:50:01 -0300

    ARQUIVOS
    Exemplos de manipulação de arquivos

---
+++ 2023-12-04 - FILES/exemplo_01.py
@@ -0,0 +1,2 @@
+capitais = ['Maceió', 'Salvador', 'Fortaleza', 'São Luís', 'João Pessoa', 
+            'Recife', 'Teresina', 'Natal', 'Aracaju']
---
+++ 2023-12-04 - FILES/exemplo_02.py
@@ -0,0 +1,8 @@
+capitais = ['Maceió', 'Salvador', 'Fortaleza', 'São Luís', 'João Pessoa', 
+            'Recife', 'Teresina', 'Natal', 'Aracaju']
+
+siglas = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE']
+
+populacao = [1018948, 2872347, 2669342, 1101884, 809015, 1695727,
+             868075, 890480, 657013]
+
