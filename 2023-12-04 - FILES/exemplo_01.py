a44ac39f67c15a3ea0995f394a12438c5f67c5de
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2023-12-04 21:37:45 -0300

    FILES
    Exemplos de manipulação de arquivos

--- 2023-12-04 - FILES/exemplo_01.py
+++ 2023-12-04 - FILES/exemplo_01.py
@@ -1,2 +1,14 @@
+# Declarando a lista
 capitais = ['Maceió', 'Salvador', 'Fortaleza', 'São Luís', 'João Pessoa', 
             'Recife', 'Teresina', 'Natal', 'Aracaju']
+
+# Abrindo o arquivo no mode de escrita (w) e aceitando
+# caracteres acentuados (utf-8)
+arqCapitais = open('nordeste_1.txt', 'w', encoding='utf-8')
+
+# Escrevendo os itens da lista no arquivo
+for c in capitais:
+    arqCapitais.write(f'{c}\n')
+
+# Fechando o arquivo
+arqCapitais.close()
--- 2023-12-04 - FILES/exemplo_02.py
+++ 2023-12-04 - FILES/exemplo_02.py
@@ -1,3 +1,4 @@
+# Declarando as listas
 capitais = ['Maceió', 'Salvador', 'Fortaleza', 'São Luís', 'João Pessoa', 
             'Recife', 'Teresina', 'Natal', 'Aracaju']
 
@@ -6,3 +7,14 @@ siglas = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'PI', 'RN', 'SE']
 populacao = [1018948, 2872347, 2669342, 1101884, 809015, 1695727,
              868075, 890480, 657013]
 
+
+# Abrindo o arquivo no mode de escrita (w) e aceitando
+# caracteres acentuados (utf-8)
+arqCapitais = open('nordeste_2.txt', 'w', encoding='utf-8')
+
+# Escrevendo os itens das listas no arquivo
+for c, s, p in zip(capitais, siglas, populacao):
+    arqCapitais.write(f'{c}/{s}:{p}\n')
+
+# Fechando o arquivo
+arqCapitais.close()
