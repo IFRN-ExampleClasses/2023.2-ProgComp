13e792aaaa6ce5ddabcdf92568f413be8a5b674c
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2023-12-11 21:13:14 -0300

    FILES
    Atualização dos exemplos de dicionários e manipulação de arquivos.

--- 2023-12-08 - FILES/exemplo_02c_json.py
+++ 2023-12-08 - FILES/exemplo_02c_json.py
@@ -1,4 +1,4 @@
-import os, requests
+import os, requests, sys
 
 # Obtendo o diretório corrente
 strDiretorio = os.path.abspath(__file__)
@@ -9,8 +9,9 @@ strDiretorio = os.path.dirname(strDiretorio)
 strURL = 'https://dados.ifrn.edu.br/dataset/7b48f9d0-205d-46b1-8225-a3cc7d3973ff/resource/fe0e9d2c-1c02-4625-b692-13edcc3380ae/download/dados_extraidos_recursos_cursos-ofertados.json'
 retRequisicao = requests.get(strURL)
 
-if retRequisicao.status_code == 200:
-    lstCursos = retRequisicao.json()
-    print(lstCursos)
-else:
-    print(f'ERRO: Erroao obter os dados...\nCÓDIGO ERRO = {retRequisicao.status_code}')
+if retRequisicao.status_code != 200:
+    print(f'ERRO: Erro ao obter os dados...\nCÓDIGO ERRO = {retRequisicao.status_code}')
+    sys.exit()
+
+lstCursos = retRequisicao.json()
+print(lstCursos)
